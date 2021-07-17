bac7b794bd8f635aa01f9e66106980187a7b2fba
Francievellyn <fls@a.palmares.ifpe.edu.br>
2021-07-17 10:30:01 -0300

    dependencias do projeto

---
+++ ListaAluno/src/main/java/br/edu/ifpe/palmares/a/fls/ListaAluno/ListaAlunoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.palmares.a.fls.ListaAluno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaAlunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaAlunoApplication.class, args);
+	}
+
+}
---
+++ ListaAluno/src/test/java/br/edu/ifpe/palmares/a/fls/ListaAluno/ListaAlunoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.palmares.a.fls.ListaAluno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaAlunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
